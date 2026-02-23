e6e72eab5828a508712693aaaadf96152a7dfe24
Marcos Sanchez <sanchez.marcos.germ@gmail.com>
2026-02-23 19:24:01 -0300

    refactor: simplify Dialog component structure by removing unused animation classes

--- frontend/components/ui/dialog.tsx
+++ frontend/components/ui/dialog.tsx
@@ -38,8 +38,6 @@ const DialogContent = React.forwardRef<
         "data-[state=open]:animate-in data-[state=closed]:animate-out",
         "data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0",
         "data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95",
-        "data-[state=closed]:slide-out-to-left-1/2 data-[state=closed]:slide-out-to-top-[48%]",
-        "data-[state=open]:slide-in-from-left-1/2 data-[state=open]:slide-in-from-top-[48%]",
         className,
       )}
       {...props}
@@ -58,10 +56,7 @@ const DialogHeader = ({
   className,
   ...props
 }: React.HTMLAttributes<HTMLDivElement>) => (
-  <div
-    className={cn("flex flex-col space-y-1.5 mb-4", className)}
-    {...props}
-  />
+  <div className={cn("flex flex-col space-y-1.5 mb-4", className)} {...props} />
 );
 DialogHeader.displayName = "DialogHeader";
 
